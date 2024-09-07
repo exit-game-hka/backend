7a6c63bd80e3a4e035947b60e435c92695fb6b98
ramses <ramsestetang2017@outlook.com>
2024-09-07 18:11:17 +0200

    Base path über DI fürs Swagger-ui gesetzt

--- src/main/java/com/hka/exitgame/ExitGameApplication.java
+++ src/main/java/com/hka/exitgame/ExitGameApplication.java
@@ -1,8 +1,15 @@
 package com.hka.exitgame;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "https://193.196.37.101/exit-game-backend", description = "Default Server URL")
+		}
+)
 @SpringBootApplication
 public class ExitGameApplication {
 
